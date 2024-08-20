0c07ee413430b426768c99e8fafc9b30c2b690fc
igitiit <jpapadem@iit.edu>
2024-08-19 23:08:47 -0500

    Add failing test case to demonstrate error

--- src/test/java/com/example/BankAccountTest.java
+++ src/test/java/com/example/BankAccountTest.java
@@ -9,13 +9,20 @@ public class BankAccountTest {
     public void testDeposit() {
         BankAccount account = new BankAccount(100);
         account.deposit(50);
-        assertEquals(150, account.getBalance(), 0.001);
+        assertEquals(150, account.getBalance(), 0.001); // This test will pass
     }
 
     @Test
     public void testWithdraw() {
         BankAccount account = new BankAccount(100);
         account.withdraw(50);
-        assertEquals(50, account.getBalance(), 0.001);
+        assertEquals(50, account.getBalance(), 0.001); // This test will pass
+    }
+
+    @Test
+    public void testDepositFail() {
+        BankAccount account = new BankAccount(100);
+        account.deposit(50);
+        assertEquals(200, account.getBalance(), 0.001); // This will fail because the actual balance is 150
     }
 }
